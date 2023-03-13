c3d9d550e9bf206a00c1402e2ef923e88f2eb7f3
Daniel Machado <cdanielmachado@gmail.com>
2023-03-13 11:41:15 +0100

    update

--- main.py
+++ main.py
@@ -1,58 +1,60 @@
 #!/usr/bin/env python
 
+#SBATCH --array=0-1439
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1            
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16000
+#SBATCH --job-name="solvers"
+
 from reframed import load_cbmodel, Environment, FBA, pFBA, minimal_medium
 from reframed.solvers.pulp_solver import PuLPSolver
 from time import time
-from argparse import ArgumentParser
+import os
 
-def main():
+models = ['iIT341', 'iML1515', 'iLB1027', 'iMM904', 'iCHOv1', 'Recon3D']
+interfaces = ['CPLEX_PY', 'GUROBI', 'GLPK_CMD', 'COIN_CMD', 'SCIP_CMD', 'HiGHS_CMD']
+tests = ['LP1', 'LP2', 'MILP1', 'MILP2']
 
-    parser = ArgumentParser()
-    parser.add_argument("model")
-    parser.add_argument("interface")
-    parser.add_argument("test")
-    parser.add_argument("--replicate", type=int, default=0)
+job_index = int(os.environ['SLURM_ARRAY_TASK_ID'])
+model_id = models[job_index % 6]
+interface = interfaces[job_index // 6 % 6]
+test = tests[job_index // 36 % 4]
+replicate = job_index // 144
 
+print(f'running: {interface}\t{model_id}\t{test}\t{replicate}')
+
+model = load_cbmodel(f'models/{model_id}.xml.gz')
+Environment.complete(model, inplace=True, max_uptake=1)
+solver = PuLPSolver(model, interface)
+
+start = time()
+
+if test == 'LP1':
+    sol = FBA(model, solver=solver, get_values=False)
+    fobj = sol.fobj
+
+if test == 'LP2':
+    sol = pFBA(model, solver=solver, cleanup=False)
+    fobj = sol.fobj       
     
-    args = parser.parse_args()
-    model_id = args.model
-    interface = args.interface
-    test = args.test
-    replicate = args.replicate
+if test == 'MILP1':
+    sol = minimal_medium(model, solver=solver, max_uptake=1, min_growth=0.01, 
+                            validate=False, warnings=False, min_mass_weight=False)
+    fobj = sol[1].fobj
+    
+if test == 'MILP2':
+    sol = minimal_medium(model, solver=solver, max_uptake=1, min_growth=0.01, 
+                            validate=False, warnings=False, min_mass_weight=True)
+    fobj = sol[1].fobj
+    
+elapsed = time() - start
 
-    model = load_cbmodel(f'models/{model_id}.xml.gz')
-    Environment.complete(model, inplace=True, max_uptake=1)
-    solver = PuLPSolver(model, interface)
+filepath = f'output/{interface}_{model_id}_{test}_{replicate}.tsv'
+header = 'interface\tmodel\ttest\treplicate\tvalue\ttime\n'
+data = f'{interface}\t{model_id}\t{test}\t{replicate}\t{fobj}\t{elapsed}\n'
 
-    start = time()
+with open(filepath, 'w') as f:
+    f.write(header)
+    f.write(data)
 
-    if test == 'FBA':
-        sol = FBA(model, solver=solver, get_values=False)
-        fobj = sol.fobj
-
-    if test == 'pFBA':
-        sol = pFBA(model, solver=solver, cleanup=False)
-        fobj = sol.fobj       
-        
-    if test == 'MM1':
-        sol = minimal_medium(model, solver=solver, max_uptake=1, min_growth=0.01, 
-                                validate=False, warnings=False, min_mass_weight=False)
-        fobj = sol[1].fobj
-        
-    if test == 'MM2':
-        sol = minimal_medium(model, solver=solver, max_uptake=1, min_growth=0.01, 
-                                validate=False, warnings=False, min_mass_weight=True)
-        fobj = sol[1].fobj
-        
-    elapsed = time() - start
-
-    filepath = f'output/{interface}_{model_id}_{test}_{replicate}.tsv'
-    header = 'interface\tmodel\ttest\treplicate\tvalue\ttime\n'
-    data = f'{interface}\t{model_id}\t{test}\t{replicate}\t{fobj}\t{elapsed}\n'
-
-    with open(filepath, 'w') as f:
-        f.write(header)
-        f.write(data)
-
-if __name__ == '__main__':
-    main()
